465deaeca2170dfc4a13036e59ba0941f9fb842c
mzahid <abdulzahid.01@gmail.com>
2025-06-05 01:28:47 -0700

    fixed the path of storageState

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     baseURL: process.env.UI_URL,
-    storageState: './storageState.json',   // <- reuse session here
+    storageState: '/auth/storageState.json',   // <- reuse session here
     screenshot: "on",
     video: "on"
   },
