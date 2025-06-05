fd57d0d4d007524d25c9dd2436b3af16fef95575
mzahid <abdulzahid.01@gmail.com>
2025-06-05 00:25:24 -0700

    Added comments

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,20 +1,16 @@
-import { Page } from "@playwright/test";
-
 export class LoginPage {
-  constructor(Page) {
-    this.page = Page;
-    this.username = this.page.locator('[data-test="email"]');
-    this.password = this.page.locator('[data-test="password"]');
-    this.submit = this.page.locator('[data-test="login-submit"]');
-    this.navMenu = this.page.locator('[data-test="nav-menu"]');
-    this.navSignOut = this.page.locator('[data-test="nav-sign-out"]');
-    this.navSignIn = this.page.locator('[data-test="nav-sign-in"]');
-    this.totp = this.page.locator("#totp");
-    this.verifyTotp = this.page.locator('[data-test="verify-totp"]');
+  constructor(page) {
+    this.page = page;
+    this.username = page.locator('[data-test="email"]');
+    this.password = page.locator('[data-test="password"]');
+    this.submit = page.locator('[data-test="login-submit"]');
+    this.navMenu = page.locator('[data-test="nav-menu"]');
+    this.navSignOut = page.locator('[data-test="nav-sign-out"]');
+    this.navSignIn = page.locator('[data-test="nav-sign-in"]');
   }
 
   async goto() {
-    await this.page.goto("/auth/login");
+    await this.page.goto('/#/auth/login');
   }
 
   async login(email, password) {
@@ -23,6 +19,7 @@ export class LoginPage {
     await this.password.fill(password);
     await this.submit.waitFor({ state: 'visible' });
     await this.submit.click();
-
   }
 }
+// This class encapsulates the login page functionality for Playwright tests.
+// It includes methods to navigate to the login page and perform login actions.
