21e70edcf651577a8f7b0a3c6596c150b6ee8681
mzahid <abdulzahid.01@gmail.com>
2025-06-05 00:27:13 -0700

    loginSetup to save the storage state , will need to move this file later to utils and global setup

--- pages/loginSetup.js
+++ pages/loginSetup.js
@@ -2,31 +2,29 @@ require('dotenv').config();
 const { chromium } = require('@playwright/test');
 
 (async () => {
-  const browser = await chromium.launch({ headless: false }); // headless false to see what's happening
+  const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
 
   await page.goto('https://practicesoftwaretesting.com/auth/login');
 
-  // Navigate to login
-  //await page.click('text=My Account');
+  const email = process.env.PLAYWRIGHT_USERNAME;
+  const password = process.env.PLAYWRIGHT_PASSWORD;
 
-  // Wait for login fields
-  await page.waitForSelector('[data-test="email"]', { timeout: 10000 });
-
-  const playwrightEmail = process.env.PLAYWRIGHT_USERNAME;
-  const playwrightPassword = process.env.PLAYWRIGHT_PASSWORD;
-
-  if (!playwrightEmail || !playwrightPassword) {
-    throw new Error("Missing PLAYWRIGHT_USERNAME or PLAYWRIGHT_PASSWORD in environment");
+  if (!email || !password) {
+    throw new Error('Missing PLAYWRIGHT_USERNAME or PLAYWRIGHT_PASSWORD in .env');
   }
 
-  await page.locator('[data-test="email"]').fill(playwrightEmail);
-  await page.locator('[data-test="password"]').fill(playwrightPassword);
+  await page.locator('[data-test="email"]').fill(email);
+  await page.locator('[data-test="password"]').fill(password);
   await page.click('[data-test="login-submit"]');
 
-  await page.waitForLoadState('networkidle');
-  await context.storageState({ path: 'storageState.json' });
+  //  Wait until you’re logged in
+  await page.waitForURL('**/account', { timeout: 10000 });
+  //await page.waitForSelector('[data-test="nav-user-menu"]', { timeout: 10000 });
+
+  //  Save the authenticated state
+  await context.storageState({ path: './storageState.json' });
 
   await browser.close();
 })();
