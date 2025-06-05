4cbd8230179cbf0f62b2e1bbe40c82cddbea78e6
mzahid <abdulzahid.01@gmail.com>
2025-06-05 00:38:36 -0700

    Included AfterEach method to logout after test case is complete

--- tests/accounts/accountLogin.spec.js
+++ tests/accounts/accountLogin.spec.js
@@ -10,4 +10,14 @@ test.describe('My Account Login Test', () => {
     expect(await page.locator('[data-test="page-title"]').innerText()).toContain('My account');
     expect(await page.locator('[data-test="nav-favorites"]').innerText()).toContain('Favorites');
   });
+
+  test.afterEach(async ({ page }) => {
+    // Ensure the user is logged out after each test
+    await page.locator('[data-test="nav-menu"]').click();
+    await page.waitForSelector('[data-test="nav-sign-out"]');
+    await page.click('[data-test="nav-sign-out"]');
+    await expect(page.locator('[data-test="nav-sign-in"]')).toBeVisible();
+    expect(await page.locator('[data-test="nav-sign-in"]').innerText()).toContain('Sign in');
+    await page.close();
+  });
 });
