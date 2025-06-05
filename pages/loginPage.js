2c4d6c140bdc4cc34f727bc854e955b486fa1139
mzahid <abdulzahid.01@gmail.com>
2025-06-05 01:47:39 -0700

    included logout method

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,16 +1,16 @@
 export class LoginPage {
   constructor(page) {
     this.page = page;
-    this.username = page.locator('[data-test="email"]');
-    this.password = page.locator('[data-test="password"]');
-    this.submit = page.locator('[data-test="login-submit"]');
-    this.navMenu = page.locator('[data-test="nav-menu"]');
-    this.navSignOut = page.locator('[data-test="nav-sign-out"]');
-    this.navSignIn = page.locator('[data-test="nav-sign-in"]');
+    this.username = this.page.locator('[data-test="email"]');
+    this.password = this.page.locator('[data-test="password"]');
+    this.submit = this.page.locator('[data-test="login-submit"]');
+    this.navMenu = this.page.locator('[data-test="nav-menu"]');
+    this.navSignOut = this.page.locator('[data-test="nav-sign-out"]');
+    this.navSignIn = this.page.locator('[data-test="nav-sign-in"]');
   }
 
   async goto() {
-    await this.page.goto('/#/auth/login');
+    await this.page.goto('https://practicesoftwaretesting.com/auth/login');
   }
 
   async login(email, password) {
@@ -28,5 +28,3 @@ export class LoginPage {
     await this.navSignIn.waitFor({ state: 'visible' });
   }
 }
-// This class encapsulates the login page functionality for Playwright tests.
-// It includes methods to navigate to the login page and perform login actions.
